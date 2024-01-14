e4bde298766b7c1938328c1642b0cd5d4cdf1172
HaHaWTH <id_cn00@outlook.com>
2024-01-15 05:09:09 +0800

    [ci skip] Update

--- src/main/java/io/wdsj/clientboomerpacketevents/Utils.java
+++ src/main/java/io/wdsj/clientboomerpacketevents/Utils.java
@@ -2,6 +2,12 @@ package io.wdsj.clientboomerpacketevents;
 
 import org.bukkit.entity.Player;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
 public class Utils {
     public static boolean isClassLoaded(String className) {
         try {
@@ -20,5 +26,34 @@ public class Utils {
      */
     public static String getPlayerIp(Player player) {
         return player.getAddress().getAddress().getHostAddress();
+    }
+
+    public static String getGithubCommit(){
+        String urlToConnect = "https://api.github.com/repos/HaHaWTH/ClientBoomer";
+
+        try {
+            URL url = new URL(urlToConnect);
+            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+            conn.setRequestMethod("GET");
+
+            int responseCode = conn.getResponseCode();
+
+            if (responseCode == HttpURLConnection.HTTP_OK) {
+                BufferedReader in = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+                String inputLine;
+                StringBuilder response = new StringBuilder();
+
+                while ((inputLine = in.readLine()) != null) {
+                    response.append(inputLine);
+                }
+                in.close();
+                return response.toString();
+            } else {
+                return null;
+            }
+
+        } catch (IOException e) {
+            return null;
+        }
     }
 }
